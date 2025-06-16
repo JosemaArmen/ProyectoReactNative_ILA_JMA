d0a472f80480e09956261dfa9d87d262d4c267e9
Ines Larrayoz <ilarrayozarrigote@gmail.com>
2025-06-16 09:47:26 +0200

    ultimo commit antes de la union con la rama nuevo_trip

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import Login from './login/Login';
 import Account from './login/Account'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
